e83ce2882000bfa796cac30209c86dce1da73adb
khaled hassan <khaled2001hassan@gmail.com>
2023-02-19 18:25:19 +0200

    Article Api Commit

--- app/src/main/java/com/example/newsapp2/Ui/ArticleActivity.kt
+++ app/src/main/java/com/example/newsapp2/Ui/ArticleActivity.kt
@@ -11,48 +11,88 @@ import com.example.newsapp.api.ApiManager
 import com.example.newsapp.model.Response
 import com.example.newsapp.model.TabsDM
 import com.example.newsapp2.R
+import com.example.newsapp2.model.ArticleResponse
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import retrofit2.Call
 import retrofit2.Callback
 
 class ArticleActivity : AppCompatActivity() {
-    lateinit var tabLayout:TabLayout
-    lateinit var progressBar :ProgressBar
+    lateinit var tabLayout: TabLayout
+    lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article)
         getTaps()
         initView()
     }
-    fun initView(){
-        tabLayout=findViewById(R.id.TapsLayout)
-        progressBar=findViewById(R.id.ProgressBar)
+
+    fun initView() {
+        tabLayout = findViewById(R.id.TapsLayout)
+        progressBar = findViewById(R.id.ProgressBar)
 
     }
-    fun getTaps(){
-        ApiManager.getApi().getTabs(ApiManager.apiKay).enqueue(object :Callback<Response>{
+
+    fun initListener() {
+        tabLayout.setOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val id = tab?.tag
+                getArticle(id.toString())
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    fun getTaps() {
+        ApiManager.getApi().getTabs(ApiManager.apiKay).enqueue(object : Callback<Response> {
             override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
-               progressBar.isVisible=false
-                Log.e( "onResponse: ","${response.body()}" )
-                if (response.body()?.code==null){
+                progressBar.isVisible = false
+                Log.e("onResponse: ", "${response.body()}")
+                if (response.body()?.code == null) {
                     showTaps(response.body()?.tabs!!)
                 }
-
 
 
             }
 
             override fun onFailure(call: Call<Response>, t: Throwable) {
-                progressBar.isVisible=false
-                Toast.makeText(this@ArticleActivity,"wrong thing",Toast.LENGTH_LONG).show()
+                progressBar.isVisible = false
+                Toast.makeText(this@ArticleActivity, "wrong thing", Toast.LENGTH_LONG).show()
             }
 
         })
     }
-    fun showTaps(tabs : List<TabsDM>){
+
+    fun getArticle(tabId: String) {
+        ApiManager.getApi().getArtical(ApiManager.apiKay, tabId)
+            .enqueue(object : Callback<ArticleResponse> {
+                override fun onResponse(
+                    call: Call<ArticleResponse>,
+                    response: retrofit2.Response<ArticleResponse>
+                ) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onFailure(call: Call<ArticleResponse>, t: Throwable) {
+                    progressBar.isVisible = false
+                    Toast.makeText(this@ArticleActivity, "wrong thing", Toast.LENGTH_LONG).show()
+                }
+            })
+
+    }
+
+    fun showTaps(tabs: List<TabsDM>) {
         tabs.forEach {
-            val newTab =tabLayout.newTab()
-            newTab.text=it.name
+            val newTab = tabLayout.newTab()
+            newTab.text = it.name
+            newTab.tag = newTab.id
             tabLayout.addTab(newTab)
         }
 
--- app/src/main/java/com/example/newsapp2/api/WepServices.kt
+++ app/src/main/java/com/example/newsapp2/api/WepServices.kt
@@ -1,6 +1,7 @@
 package com.example.newsapp.api
 
 import com.example.newsapp.model.Response
+import com.example.newsapp2.model.ArticleResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,4 +12,10 @@ interface WepServices {
         @Query("apiKey")key:String
 
     ):Call<Response>
+    @GET("/v2/everything")
+    fun getArtical(
+        @Query("apiKey")key:String,
+        @Query("sources")tab:String
+
+    ):Call<ArticleResponse>
 }
